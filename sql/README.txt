007fb5b2e457270717fc03e9374ac8a218ad79eb
Ondrej Platek <ondrej.platek at seznam dot cz>
2011-11-23 09:15:47 +0100

    1)Documentation transferred to Latex 2) added sql population scripts for postgres sqpl -> todo rewrite it to oracle

---
+++ sql/README.txt
@@ -0,0 +1,7 @@
+artpop.sql, sub.sql and advert.sql are CREATE TABLE scripts for Article Popularity, Subscription and Advertisement facts.
+
+artpop.data.sql, sub.data.sql and advert.data.sql contain SQL INSERT statements with some sample data.
+
+artpop.php, sub.php and advert.php are PHP scripts that generate SQL with INSERT statements. You can modify some of the variables in these files to generate even more data.
+
+sub.canada.csv contains Canadian addresses & cities used by sub.php to generate sample data.
---
+++ sql/artpop.sql
@@ -0,0 +1,49 @@
+﻿DROP SCHEMA IF EXISTS artpop CASCADE;
+CREATE SCHEMA artpop;
+
+CREATE TABLE artpop.Date (
+   keyDate serial NOT NULL,
+   Hour smallint NOT NULL,
+   Day smallint NOT NULL,
+   Month smallint NOT NULL,
+   Year smallint NOT NULL,
+   PRIMARY KEY (keyDate)
+);
+
+CREATE TABLE artpop.Article (
+   keyArticle serial NOT NULL,
+   Title text NOT NULL,
+   Subcategory text NOT NULL,
+   Category text NOT NULL,
+   Author text NOT NULL,
+   AuthorExperience text NOT NULL,
+   AuthorDepartment text NOT NULL,
+   PublicationdDay smallint NOT NULL,
+   PublicationMonth smallint NOT NULL,
+   PublicationYear smallint NOT NULL,
+   PRIMARY KEY (keyArticle)
+);
+
+CREATE TABLE artpop.Tag
+(
+   keyTag serial NOT NULL,
+   Tag text NOT NULL,
+   PRIMARY KEY (keyTag)
+);
+
+CREATE TABLE artpop.ArticleTags
+(
+   keyTag integer REFERENCES artpop.Tag(keyTag) NOT NULL,
+   keyArticle integer REFERENCES artpop.Article(keyArticle) NOT NULL,
+   PRIMARY KEY (keyTag, keyArticle)
+);
+
+CREATE TABLE artpop.ArticlePopularity
+(
+   keyDate integer REFERENCES artpop.date(keyDate) NOT NULL,
+   keyArticle integer REFERENCES artpop.article(keyArticle) NOT NULL,
+   Reads integer NOT NULL,
+   Shares integer NOT NULL,
+   Comments integer NOT NULL,
+   PRIMARY KEY (keyDate, keyArticle)
+);
