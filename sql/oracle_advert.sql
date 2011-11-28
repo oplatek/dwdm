d3ab2d139b305cc1ed2c08807f9c70fe913588cb
Ondrej Platek <ondrej.platek at seznam dot cz>
2011-11-28 10:32:27 +0100

    created copies for rewriting sql scripts from postgres syntax to oracle syntax. The copies start with oracle_MYCOPY.sql

---
+++ sql/oracle_advert.sql
@@ -0,0 +1,32 @@
+﻿-- DROP SCHEMA IF EXISTS advert CASCADE; 
+-- oracle cannot drop schema without drop user priviliges -> schema is associeted with user according https://forums.oracle.com/forums/thread.jspa?threadID=505126
+CREATE SCHEMA advert;
+
+CREATE TABLE advert.Date
+(
+   keyDate serial NOT NULL,
+   Day smallint NOT NULL,
+   Month smallint NOT NULL,
+   Quarter smallint NOT NULL,
+   Year smallint NOT NULL,
+   PRIMARY KEY (keyDate)
+);
+
+CREATE TABLE advert.Campaign
+(
+   keyCampaign serial NOT NULL,
+   Name text NOT NULL,
+   AdvertiserName text NOT NULL,
+   AdvertiserCategory text NOT NULL,
+   PRIMARY KEY (keyCampaign)
+);
+
+CREATE TABLE advert.Advertisement
+(
+   keyDate integer REFERENCES advert.Date(keyDate) NOT NULL,
+   keyCampaign integer REFERENCES advert.Campaign(keyCampaign) NOT NULL,
+   Revenue numeric(20,6) NOT NULL,
+   Displays integer NOT NULL,
+   Clicks integer NOT NULL,
+   PRIMARY KEY (keyDate, keyCampaign)
+);
---
+++ sql/oracle_sub.sql
@@ -0,0 +1,46 @@
+﻿DROP SCHEMA IF EXISTS sub CASCADE;
+CREATE SCHEMA sub;
+
+CREATE TABLE sub.Date
+(
+   keyDate serial NOT NULL,
+   Day smallint NOT NULL,
+   Month smallint NOT NULL,
+   Year smallint NOT NULL,
+   Week smallint NOT NULL,
+   Thanksgiving boolean NOT NULL,
+   Halloween boolean NOT NULL,
+   Easter boolean NOT NULL,
+   Christmas boolean NOT NULL,
+   PRIMARY KEY (keyDate)
+);
+
+CREATE TABLE sub.Location
+(
+   keyLocation serial NOT NULL,
+   City text NOT NULL,
+   State text NOT NULL,
+   Country text NOT NULL,
+   Zone text NOT NULL,
+   Continent text NOT NULL,
+   PRIMARY KEY (keyLocation)
+);
+
+CREATE TABLE sub.Period
+(
+  keyPeriod serial NOT NULL,
+  Name text NOT NULL,
+  PRIMARY KEY (keyPeriod)
+);
+
+CREATE TABLE sub.Subscription
+(
+   keyDate integer REFERENCES sub.Date(keyDate) NOT NULL,
+   keyLocation integer REFERENCES sub.Location(keyLocation) NOT NULL,
+   keyPeriod integer REFERENCES sub.Period(keyPeriod) NOT NULL,
+   Quantity smallint NOT NULL,
+   Price numeric(5,2) NOT NULL,
+   Discount numeric(6,5) NOT NULL,
+   PRIMARY KEY (keyDate, keyLocation, keyPeriod)
+);
+
