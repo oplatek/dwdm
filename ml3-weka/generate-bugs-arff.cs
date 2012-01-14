1d0d3611a8719d97b8979cf7ac32e2c10dea324d
Ondrej Platek <ondrej.platek at seznam dot cz>
2012-01-14 16:58:52 +0100

    generated with strings

--- ml3-weka/generate-bugs-arff.cs
+++ ml3-weka/generate-bugs-arff.cs
@@ -40,30 +40,30 @@ namespace Dwdm
             using (StreamWriter sw = new StreamWriter(target))
             {
                 sw.WriteLine("@relation bugs");
-                sw.WriteLine("@attribute id numeric");
-                sw.WriteLine("@attribute title string");
-                sw.WriteLine("@attribute status {" + string.Join(", ", bugs.Select(b => b.Status).Distinct()) + "}");
-                sw.WriteLine("@attribute type {" + string.Join(", ", bugs.Select(b => b.Type).Distinct()) + "}");
-                sw.WriteLine("@attribute priority {" + string.Join(", ", bugs.Select(b => b.Priority).Distinct()) + "}");
-                sw.WriteLine("@attribute stars numeric");
-                sw.WriteLine("@attribute comments numeric");
-                sw.WriteLine("@attribute description string");
-                sw.WriteLine("@attribute owner {" + string.Join(", ", bugs.Select(b => b.Owner).Distinct()) + "}");
-                sw.WriteLine("@attribute component {" + string.Join(", ", bugs.Select(b => b.Component).Distinct()) + "}");
+                sw.WriteLine("@attribute bug-id numeric");
+                sw.WriteLine("@attribute bug-title string");
+                sw.WriteLine("@attribute bug-status {" + string.Join(", ", bugs.Select(b => b.Status).Distinct()) + "}");
+                sw.WriteLine("@attribute bug-type {" + string.Join(", ", bugs.Select(b => b.Type).Distinct()) + "}");
+                sw.WriteLine("@attribute bug-priority {" + string.Join(", ", bugs.Select(b => b.Priority).Distinct()) + "}");
+                sw.WriteLine("@attribute bug-stars numeric");
+                sw.WriteLine("@attribute bug-comments numeric");
+                sw.WriteLine("@attribute bug-description string");
+                sw.WriteLine("@attribute bug-owner {" + string.Join(", ", bugs.Select(b => b.Owner).Distinct()) + "}");
+                sw.WriteLine("@attribute bug-component {" + string.Join(", ", bugs.Select(b => b.Component).Distinct()) + "}");
                 sw.WriteLine();
                 sw.WriteLine("@data");
 
                 foreach (var bug in bugs.Where(b => b.Priority != "" && b.Type != "" && !b.Title.EndsWith("\\") && !b.Description.EndsWith("\\")))
                 {
                     sw.WriteLine(string.Format(
-                        //"{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
+                        "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
                         //"{0},{1}",
-                        "{0},{1},{2},{3},{4},{5},{6},{7}",
+//                        "{0},{1},{2},{3},{4},{5},{6},{7}",
                         bug.ID, 
-                        //"'" + bug.Title.Replace("'", @"\'").Replace("\n", " ") + "'",
+                        "'" + bug.Title.Replace("'", @"\'").Replace("\n", " ") + "'",
                         bug.Status, bug.Type, bug.Priority, 
                         bug.Stars, bug.Comments,
-                        //"'" + bug.Description.Replace("'", @"\'").Replace("\n", " ") + "'"
+                        "'" + bug.Description.Replace("'", @"\'").Replace("\n", " ") + "'",
                         bug.Owner,
                         bug.Component != "null" ? bug.Component : "null"
 					));
